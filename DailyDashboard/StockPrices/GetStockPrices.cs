89a8b136eb46558f3d78d3ac63911b3a81c68b00
Candice Groce <cgroce13@live.com>
2020-09-04 16:24:04 -0500

    Adding functions api for getting stock price

---
+++ DailyDashboard/StockPrices/GetStockPrices.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Linq.Expressions;
+
+namespace StockPrices
+{
+    public static class GetStockPrices
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        [FunctionName("GetStockPrices")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            
+            //ensure we have a stock symbol to look up
+            string stockSymbol = req.Query["symbol"];
+            if (String.IsNullOrEmpty(stockSymbol))
+                return new BadRequestObjectResult("Please provide a stock symbol");
+
+            //api only accepts uppercase
+            stockSymbol = stockSymbol.ToUpper();
+            
+            //ensure we have a token to call the api
+            string apiToken = System.Environment.GetEnvironmentVariable("apitoken");
+            if (string.IsNullOrEmpty(apiToken))
+                return new BadRequestObjectResult("no api token provided");
+
+            //make the api call
+            client.DefaultRequestHeaders.Accept.Clear();
+            var stringTask = client.GetStringAsync(String.Format("https://finnhub.io/api/v1/quote?symbol={0}&token={1}",stockSymbol, apiToken));
+            var msg = await stringTask;
+            
+            StockPriceResult result = JsonConvert.DeserializeObject<StockPriceResult>(msg);
+
+            
+            return result.CurrentPrice > 0
+                ? (ActionResult)new OkObjectResult(String.Format("CurrentPrice of {0} is {1}", stockSymbol, result.CurrentPrice))
+                : new BadRequestObjectResult("Error getting price data");
+        }
+    }
+}
---
+++ DailyDashboard/StockPrices/StockPriceResult.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace StockPrices
+{
+    public class StockPriceResult
+    {
+        [JsonProperty("c")]
+        public double CurrentPrice { get; set; }
+
+        [JsonProperty("h")]
+        public double HighPrice { get; set; }
+
+        [JsonProperty("l")]
+        public double LowPrice { get; set; }
+
+        [JsonProperty("o")]
+        public double OpeningPrice { get; set; }
+
+        [JsonProperty("pc")]
+        public double PreviousClose { get; set; }
+
+
+    }
+}
